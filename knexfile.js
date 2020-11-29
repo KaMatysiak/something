01211228616551b03979bdafb28ca31cd7da635c
Kalina Matysiak <kalinamatysiak3@gmail.com>
2020-11-29 21:27:47 +0100

    change knexfile

--- knexfile.js
+++ knexfile.js
@@ -4,8 +4,8 @@ module.exports = {
     client: 'pg',
     connection: {
       host: '127.0.0.1',
-      user: 'postgres',
-      password: 'KaWa250',
+      user: 'admin',
+      password: 'admin',
       database: 'moto-app'
     },
     migrations: {
